7c20ed8d4dca8dc4668100b6f74d7d39a326b7e4
DuLLatJatt <142017979+Dullat@users.noreply.github.com>
2024-06-20 22:42:35 +0530

    Update binarySearchTree.js

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -32,6 +32,46 @@ class BinarySearchTree {
       } else this.insertNode(node.right, newNode)
     }
   }
+
+  inorder(node) {
+    if (node === null) {
+      return null
+    } else {
+      this.inorder(node.left)
+      console.log(node.data)
+      this.inorder(node.right)
+    }
+  }
+
+  preorder(node) {
+    if (node === null) {
+      return null
+    } else {
+      console.log(node.data)
+      this.preorder(node.left)
+      this.preorder(node.right)
+    }
+  }
+
+  postorder(node) {
+    if (node === null) {
+      return null
+    } else {
+      this.postorder(node.left)
+      this.postorder(node.right)
+      console.log(node.data)
+    }
+  }
+
+  search(node, data) {
+    if (node === null) {
+      return null
+    } else if (data < node.data) {
+      return this.search(node.left, data)
+    } else if (data > node.data) {
+      return this.search(node.right, data)
+    } else return node
+  }
 }
 
 let tree = new BinarySearchTree()
@@ -42,4 +82,6 @@ tree.insert(14)
 tree.insert(23)
 tree.insert(25)
 
-console.log(tree)
+console.log(tree.search(tree.root, 2))
+
+tree.postorder(tree.root)
